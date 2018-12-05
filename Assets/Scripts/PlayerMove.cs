7702cc41f520b883936ba714c9659799920d0671
Gragog <36130475+Gragog@users.noreply.github.com>
2018-12-05 19:38:48 +0100

    Movement-Script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    [Range(-25f, 25f)]
+    public float speed = 10f;
+
+    KeyCode moveRight  = KeyCode.D;
+    KeyCode moveLeft   = KeyCode.A;
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 movement = new Vector3();
+
+        if (Input.GetKey(moveRight))
+        {
+            movement.x += speed;
+        }
+
+        if (Input.GetKey(moveLeft))
+        {
+            movement.x -= speed;
+        }
+
+        transform.Translate(movement * Time.deltaTime, Space.World);
+    }
 }
