7ede85f967d007a675b470dca17a5d64f804792b
Gragog <36130475+Gragog@users.noreply.github.com>
2018-12-05 19:04:23 +0100

    BasicTileset

---
+++ Assets/Editor/EditorShortcuts.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class EditorShortCuts
+{
+    [MenuItem("Edit/Save and Play #LEFT+_F5")] // shortcut key F5 to Play (and exit playmode also)
+    static void SaveAndPlay()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+        }
+
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("Edit/Run _F5")] // shortcut key F5 to Play (and exit playmode also)
+    static void Play()
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+
+    }
+}
