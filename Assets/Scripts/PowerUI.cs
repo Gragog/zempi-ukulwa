0b611ef5df7f7489cb2c2b42459d5baaed8049c6
Gragog <36130475+Gragog@users.noreply.github.com>
2018-12-30 18:28:15 +0100

    Can change shoot direction using mouse

--- Assets/Scripts/PowerUI.cs
+++ Assets/Scripts/PowerUI.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PowerUI : MonoBehaviour {
-
+public class PowerUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
     public Transform power;
 
     Camera cam;
+    bool hovering = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovering = false;
+    }
 
     private void Start()
     {
         cam = Camera.main;
     }
 
-    private void OnMouseEnter()
+    private void Update()
     {
-        Debug.Log("hi");
+        if (hovering && Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition).ToVector2();
+            Vector2 diff = mousePosition - transform.position.ToVector2();
+            Debug.Log(diff.sqrMagnitude);
 
-        Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition).ToVector2();
-
-        power.position = newPosition;
+            if (diff.sqrMagnitude < 7.5f)
+            {
+                power.position = mousePosition;
+            }
+        }
     }
 }
