ab918136adfeb953b02219ceb08ee2b16542123f
Gragog <36130475+Gragog@users.noreply.github.com>
2018-12-30 18:13:42 +0100

    added files

---
+++ Assets/Scripts/Extensions/Vector3Ext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Ext { 
+    public static Vector2 ToVector2(this Vector3 v3)
+    {
+        return new Vector2(v3.x, v3.y);
+    }
+}
---
+++ Assets/Scripts/PowerUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUI : MonoBehaviour {
+
+    public Transform power;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("hi");
+
+        Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition).ToVector2();
+
+        power.position = newPosition;
+    }
+}
