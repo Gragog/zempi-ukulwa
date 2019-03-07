af52e75fe9c416e87ef777d2b283a86dca9cf22a
Gragog <36130475+Gragog@users.noreply.github.com>
2019-03-07 18:58:56 +0100

    added files...

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    #region Singleton
+    public static RoundManager Instance
+    {
+        get;
+        private set;
+    }
+    #endregion
+
+    public byte currentPlayerIndex;
+
+    List<PlayerMove> players = new List<PlayerMove>();
+
+    public RoundManager()
+    {
+        Instance = this;
+    }
+
+    public PlayerMove CurrentPlayer
+    {
+        get { return players[currentPlayerIndex]; }
+        private set { }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayerIndex = 0;
+
+        CurrentPlayer.SetActiveTurn(true);
+    }
+
+    public void NextTurn()
+    {
+        CurrentPlayer.SetActiveTurn(false);
+
+        currentPlayerIndex = (byte)((currentPlayerIndex + 1) % players.Count);
+
+        CurrentPlayer.SetActiveTurn(true);
+    }
+
+    public void AddPlayer(PlayerMove player)
+    {
+        players.Add(player);
+    }
+}
